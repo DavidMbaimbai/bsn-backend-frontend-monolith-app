5ef59577283c910bc931f45b9c3057b96f993d58
David Mbaimbai <dmbaimbai@csod.com>
2024-05-17 16:43:38 +0200

    ui

--- book-network-ui/src/app/app-routing.module.ts
+++ book-network-ui/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {RegisterComponent} from './pages/register/register.component';
-import {authGuard} from './services/guard/auth.guard';
+import {AuthGuard} from './services/guard/auth.guard';
 import {ActivateAccountComponent} from './pages/activate-account/activate-account.component';
 
 const routes: Routes = [
@@ -21,7 +21,7 @@ const routes: Routes = [
   {
     path: 'books',
     loadChildren: () => import('./modules/book/book.module').then(m => m.BookModule),
-    canActivate: [authGuard]
+    canActivate: [AuthGuard]
   }
 ];
 
